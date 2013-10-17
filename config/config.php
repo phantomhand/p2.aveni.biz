ed78c9cdbb9b1373a81aa5fa39e49054a3d9780e
Frank Aveni <frank@phantomhand.com>
2013-10-16 21:20:23 -0400

    update email info in config

--- config/config.php
+++ config/config.php
@@ -18,7 +18,7 @@ If you want a different default time format for this app, set it below
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
-	define('APP_EMAIL', 'frank@phantomhand.com'); 
+	define('APP_EMAIL', 'frank@aveni.biz'); 
 
 /* 
 A email designated to receive messages from the server. Examples:
@@ -28,7 +28,7 @@ A email designated to receive messages from the server. Examples:
  	
  	Some might want this to be the same as the APP_EMAIL, others might want to create a designated gmail address for it
 */ 	
-	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
+	define('SYSTEM_EMAIL', 'frankaveni@gmail.com'); 
 
 # Default DB name for this app
 	define('DB_NAME', "avenibiz_p2_aveni_biz"); 

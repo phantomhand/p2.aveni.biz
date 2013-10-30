6078bf034742574a84c5f5ef5119be1f8b702dd6
phantomhand <frank@phantomhand.com>
2013-10-30 01:34:57 -0400

    work on preventing duplicate signups - add error messgae

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -6,22 +6,30 @@
 			
 		    First Name<br>
 		    <input type='text' name='first_name' class="input">
-		    <br><br>
+		    <br>
 		
 		    Last Name<br>
 		    <input type='text' name='last_name' class="input">
-		    <br><br>
+		    <br>
 		
 		    Email<br>
 		    <input type='text' name='email' class="input">
-		    <br><br>
+		    <br>
 		
 		    Password<br>
 		    <input type='password' name='password' class="input">
-		    <br><br>
+		    <br>
+		    <br>
 		
 		    <input type='submit' value='Sign up'>
 	
 		</form>
+		
+		<?php if (isset ($error) ): ?>
+		        <div class='error' style="color: red;">
+		            You're already signed up! Why not <a href="/users/login">log in</a>?
+		        </div>
+		        <br>
+		<?php endif; ?>
 	</article>
 </aside>

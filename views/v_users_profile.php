2ab02f217f3a71f42e65f01e1daaa14bc80f9176
phantomhand <frank@phantomhand.com>
2013-10-30 01:34:02 -0400

    add image upload form

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,16 +1,31 @@
 <aside id="featured" class="body">
 	<article>
 		<figure>
-			<img src="../images/avatar-question.jpg" alt="Blabbr User Pic" height="200" width="200" />
+			<img src="../uploads/avatars/avatar-<?=$user->user_id?>.png" alt="Blabbr User Pic" height="200" width="200"/>
+			
+			<!-- ../images/avatar-question.jpg-->
+
+			<form method='POST' enctype="multipart/form-data" action='/users/add_image/'>
+				<input type='file' name='image'>
+				<input type='submit' class="button">
+			</form>
 		</figure>
-			<hgroup>
-				<h2><?=$user->first_name?>'s Profile</h2>
-			</hgroup>
-			<p><strong>Name:</strong> <?=$user->first_name?> <?=$user->last_name?><br>
-			<strong>Email:</strong> <?=$user->email?></p>
-			
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium orci a molestie commodo. Aliquam sed metus libero. Maecenas adipiscing tempus accumsan. Mauris venenatis dictum arcu at interdum. Vestibulum fermentum augue id scelerisque egestas. Nam malesuada, mauris placerat malesuada consectetur.</p>
-			
+		
+		<hgroup>
+			<h2><?=$user->first_name?>'s Profile</h2>
+		</hgroup>
+		
+		<p>
+		<strong>Name:</strong> <?=$user->first_name?> <?=$user->last_name?><br>
+		<strong>Email:</strong> <?=$user->email?>
+		</p>
+		
+		<p>
+		 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium orci a molestie commodo. 
+		 Aliquam sed metus libero. Maecenas adipiscing tempus accumsan. Mauris venenatis dictum arcu at interdum. 
+		 Vestibulum fermentum augue id scelerisque egestas. Nam malesuada, mauris placerat malesuada consectetur.
+		</p>
+	
 	</article>
 </aside><!-- /#featured -->
 

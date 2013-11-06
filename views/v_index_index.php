5672168e660c968916121ddcd4abca603b289a85
phantomhand <frank@phantomhand.com>
2013-11-05 20:15:17 -0500

    replace homepage lorem ipsum text with real blurb

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,43 +1,72 @@
 <aside id="featured" class="body home">
 	<article>
 	
-		<div class="signup">
-			<h2>Welcome to <?=APP_NAME?>!</h2>
-						
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium orci a molestie commodo. 
-			Aliquam sed metus libero. Maecenas adipiscing tempus accumsan.</p>
+		<?php if (!$user): ?> <!-- If visitor is not logged in, show Sign up and Log in options -->
 			
-			<form method='POST' action='/users/signup'><input type="submit" value="Sign up" class="button">
-			</form>
-		    
-		</div>
-		
-		<div class="login">
-		
-			<h2>Log In</h2>
-			
-		 	<form method='POST' action='/users/p_login'>
-			    <p><b>Email</b><br>
-				    <input type='email' name='email' class="input" required title="Must be a valid email address">
-				    <br>		
-				    <b>Password</b><br>
-				    <input type='password' name='password' class="input" required>
-				    <br>
-			    </p>
-
-			    <input type='submit' value='Log in' class="button">
-			   </form>
-			   
-			<br>
+			<div class="signup"> <!-- Sign up form -->
+				
+				<h2>Welcome to <?=APP_NAME?>!</h2>
+							
+				<p>Got something on your mind? Don't just think it to yourself... 
+				Blab it to anyone who will listen! You can also follow others and see what they are Blabbing about.
+				So, what are you waiting for? Sign up now and get to Blabbing!</p>
+				
+				<form method='POST' action='/users/signup'><input type="submit" value="Sign up" class="button">
+				</form>
 			    
-		    <?php if (isset ($error) ): ?>
-		        <div class='error' style="color: red;">
-		            <p><strong>Login failed.<br>
-		            Please check your email and password.</strong></p>
-		            <?php echo $error ?>
-		        </div>
-		        <br>
-		    <?php endif; ?>
-		 </div>
+			</div>
+			
+			<div class="login"> <!-- Log in form -->
+			
+				<h2>Log In</h2>
+				
+			 	<form method='POST' action='/users/p_login'>
+				    <p><b>Email</b><br>
+					    <input type='email' name='email' class="input" required title="Must be a valid email address">
+					    <br>		
+					    <b>Password</b><br>
+					    <input type='password' name='password' class="input" required>
+					    <br>
+				    </p>
+				    <input type='submit' value='Log in' class="button">
+				</form>
+				   
+				<br>
+				    
+			    <?php if (isset ($error) ): ?> <!-- Show an error if the login failed -->
+			        <div class='error'>
+			            <p><strong>Login failed.<br>
+			            Please check your email and password.</strong></p>
+			    <?php echo $error ?>
+			</div>
+			
+			<br>
+			        
+		<?php endif; ?>
+		    
+		<?php else:?> <!-- If user is logged in, show a greeting and a list of features -->
+		
+			<div class="signup"> <!-- Greeting and list -->
+			
+				<h2><?php echo $user->first_name; ?>, this is your <?=APP_NAME?>!</h2>
+					
+				<p><b>What would you like to do?<br>
+				You can:</b></p>			
+				<ul>
+					<li>View <a href="/users/profile">your profile</a> and add an image</li>
+					<li>See <a href="/posts/users">who is Blabbing</a></li>
+					<li>Follow <a href="/posts/users">other Blabbrs</a> (and don't forget yourself!)</li>
+					<li>Check out the <a href="/posts">latest Blabbs</a></li>			
+				</ul>
+			</div>
+			
+			<div class="login"> <!-- Picture -->
+			
+			<a href="/posts/users"><img src="/images/user-grid-mini.jpg" alt="Blabbr User Grid" height="184" width="360"></a>
+				
+			</div>
+			
+		<?php endif; ?>		
+		 
 	</article>
 </aside>
